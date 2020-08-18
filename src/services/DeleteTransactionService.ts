0a335d1618ec98f154b64f0631815826d2a67ef5
Rick M <contato.rickmachado@gmail.com>
2020-08-18 09:32:00 -0300

    Erro no CreateTransactionService.ts

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,22 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+
+import AppError from '../errors/AppError';
+
+import Transactionsrepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
+  public async execute(id: string): Promise<void> {
     // TODO
+
+    const transactionsRepository = getCustomRepository(Transactionsrepository);
+
+    const transaction = await transactionsRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Esta transação não existe');
+    }
+
+    await transactionsRepository.remove(transaction);
   }
 }
 
